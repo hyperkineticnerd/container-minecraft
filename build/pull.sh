edf37a9aaed99adcaabe0422e5cdef4e74ae4a1b
Sean Nelson <hyperkineticnerd@gmail.com>
2022-03-15 09:41:18 -0600

    attempt to slim containers

--- build/pull.sh
+++ build/pull.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 podman pull registry.access.redhat.com/ubi8/ubi-minimal
-podman pull registry.access.redhat.com/ubi8/openjdk-11-runtime
-podman pull registry.access.redhat.com/ubi8/openjdk-17-runtime
+# podman pull registry.access.redhat.com/ubi8/openjdk-11-runtime
+# podman pull registry.access.redhat.com/ubi8/openjdk-17-runtime
