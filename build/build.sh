c8a3105a80d2198c01e5b3cbfe8e18f0063837b6
Sean Nelson <hyperkineticnerd@gmail.com>
2022-03-15 08:41:47 -0600

    Rebase Container on UBI8

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+
+declare mc_version=1.18.2
+
 podman build \
-    -t quay.io/hyperkineticnerd/minecraft:1.18.2-fabric \
-    -f Dockerfile.fabric .
+    -t quay.io/hyperkineticnerd/java:17-minecraft \
+    -f Containerfile.java-17 .
 podman build \
-    -t quay.io/hyperkineticnerd/minecraft:1.18.2-shadowman \
-    -f Dockerfile.shadowman .
+    -t quay.io/hyperkineticnerd/java:11-minecraft \
+    -f Containerfile.java-11 .
+podman build \
+    -t quay.io/hyperkineticnerd/java:8-minecraft \
+    -f Containerfile.java-8 .
+podman build \
+    -t quay.io/hyperkineticnerd/minecraft:${mc_version}-fabric \
+    -f Containerfile.fabric .
+podman build \
+    -t quay.io/hyperkineticnerd/minecraft:${mc_version}-shadowman \
+    -f Containerfile.shadowman .
--- build/push.sh
+++ build/push.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+podman push quay.io/hyperkineticnerd/java:17-minecraft
 podman push quay.io/hyperkineticnerd/minecraft:1.18.2-fabric
 podman push quay.io/hyperkineticnerd/minecraft:1.18.2-shadowman
